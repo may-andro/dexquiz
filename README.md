ad192229784a9bb0c6c0dfd7868e0fdedaa8df8c
may-andro <mayank271993@gmail.com>
2023-09-19 08:39:52 +0200

    android Firebase setup.

--- README.md
+++ README.md
@@ -4,4 +4,11 @@ A quiz on pokemon
 
 ## Getting Started
 
-TODO
+## Flavoring
+
+The app has two flavors `Kanto` & `Johto`.
+For more detail refer to the [PR](https://github.com/may-andro/dexquiz/pull/1). 
+For further read, visit [here](https://docs.flutter.dev/deployment/flavors).
+Also checkout this [video](https://www.youtube.com/watch?v=Vhm1Cv2uPko) to see in action.
+
+## Firebase
--- lib/run_flavor.dart
+++ lib/run_flavor.dart
@@ -2,12 +2,21 @@ import 'package:dexquiz/build_config.dart';
 import 'package:dexquiz/dexquiz_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 
 Future<void> runFlavor(final BuildConfig buildConfig) async {
   WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-  ]);
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
+  await Firebase.initializeApp();
+
+  await FirebaseAnalytics.instance.logEvent(
+    name: 'testing',
+    parameters: <String, dynamic>{
+      'flavor': buildConfig.buildFlavor.name,
+    },
+  );
 
   runApp(const DexQuizApp());
 }
