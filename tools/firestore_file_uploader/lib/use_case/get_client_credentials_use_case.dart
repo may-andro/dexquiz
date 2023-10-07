3a2b1e3f0906a583d976063865eb6baa7ee5c33d
may-andro <mayank271993@gmail.com>
2023-10-07 23:18:07 +0200

    test46

--- tools/firestore_file_uploader/lib/use_case/get_client_credentials_use_case.dart
+++ tools/firestore_file_uploader/lib/use_case/get_client_credentials_use_case.dart
@@ -7,11 +7,11 @@ class GetClientCredentialsUseCase {
   const GetClientCredentialsUseCase();
 
   Future<ServiceAccountCredentials> execute(String filePath) async {
-    print('GetClientCredentialsUseCase.execute $filePath');
+    print('Path to Upload $filePath');
     final credentialsFile = await File(filePath).readAsString();
-    print('GetClientCredentialsUseCase.execute $credentialsFile');
+    print('File content $credentialsFile');
     final credentialsJson = json.decode(credentialsFile);
-    print('GetClientCredentialsUseCase.execute $credentialsJson');
+    print('credentialsJson $credentialsJson');
     return ServiceAccountCredentials.fromJson(credentialsJson);
   }
 }
