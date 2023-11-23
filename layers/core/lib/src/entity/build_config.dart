79867fab6fd6901e426106a80d1401b89f16236b
may-andro <mayank271993@gmail.com>
2023-11-23 22:25:42 +0100

    Feature: Module Configurator

--- layers/core/lib/src/entity/build_config.dart
+++ layers/core/lib/src/entity/build_config.dart
@@ -1,8 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase/firebase.dart';
 
 enum BuildEnvironment { debug, qa, prod }
 
-enum BuildFlavor { kanto, jhoto }
+enum BuildFlavor { kanto, johto }
 
 class BuildConfig {
   BuildConfig({
@@ -10,10 +10,12 @@ class BuildConfig {
     required this.buildFlavor,
     required this.firebaseProjectName,
     required this.firebaseOptions,
+    required this.isCrashlyticsEnabled,
   });
 
   final BuildEnvironment buildEnvironment;
   final BuildFlavor buildFlavor;
   final String firebaseProjectName;
   final FirebaseOptions firebaseOptions;
+  final bool isCrashlyticsEnabled;
 }
