699659b5a157ac3c22d984f99db1049127a9a0b8
may-andro <mayank271993@gmail.com>
2023-11-30 20:42:58 +0100

    adds app exceptions

---
+++ layers/error_reporter/test/mock/src/exception/test_app_exception.dart
@@ -0,0 +1,3 @@
+import 'package:error_reporter/src/exception/app_exception.dart';
+
+class TestAppException implements AppException {}
---
+++ layers/error_reporter/test/mock/src/exception/test_fatal_exception.dart
@@ -0,0 +1,3 @@
+import 'package:error_reporter/src/exception/fatal_exception.dart';
+
+class TestFatalException implements FatalException {}
