9f53012a27ac54de26898bc00997c52c8d65abe7
may-andro <mayank271993@gmail.com>
2023-12-17 22:15:50 +0100

    adds foundation: color

---
+++ layers/design_system/lib/src/foundations/colors/ds_color_palette.dart
@@ -0,0 +1,11 @@
+import 'package:design_system/src/foundations/colors/ds_color_scheme.dart';
+
+abstract interface class DSColorPalette {
+  BrandColorScheme get brand;
+
+  SemanticColorScheme get semantic;
+
+  NeutralColorScheme get neutral;
+
+  BackgroundColorScheme get background;
+}
