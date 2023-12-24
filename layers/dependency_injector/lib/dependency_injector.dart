96ad267092fc26c708d8d0f2b513708af3e31e10
may-andro <mayank271993@gmail.com>
2023-12-24 16:06:52 +0100

    fixes CI error

--- layers/dependency_injector/lib/dependency_injector.dart
+++ layers/dependency_injector/lib/dependency_injector.dart
@@ -3,4 +3,3 @@ library dependency_injector;
 export 'src/configurator/module_configurator.dart';
 export 'src/locator/service_locator.dart';
 export 'src/main.dart';
-export 'package:meta/meta.dart';
