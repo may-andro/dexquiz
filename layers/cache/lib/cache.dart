53064898f9bcba9f9bbc463024da09f854d44dbf
may-andro <mayank271993@gmail.com>
2023-12-28 21:14:25 +0100

    add temp cache

---
+++ layers/cache/lib/cache.dart
@@ -0,0 +1,6 @@
+library cache;
+
+export 'src/module_configurator.dart';
+export 'src/cache_system/cache_system.dart';
+export 'package:hive/hive.dart';
+export 'package:shared_preferences/shared_preferences.dart';
---
+++ layers/cache/lib/src/cache_system/cache_system.dart
@@ -0,0 +1,3 @@
+export 'db_cache.dart';
+export 'key_value_cache.dart';
+export 'memory_cache.dart';
